8a9c5ddd023503a65b2b6eb8143e4056ab1e6714
Jan <koelzerjan@gmail.com>
2024-12-17 21:55:56 +0100

    2024 Day 1

---
+++ src/_2024/2024-01.kt
@@ -0,0 +1,28 @@
+package _2024
+
+import java.io.File
+import kotlin.math.abs
+
+fun one(intLeftSide: List<Int>, intRightSide: List<Int>): Int {
+    val sortedLeft = intLeftSide.sorted()
+    val sortedRight = intRightSide.sorted()
+
+    return sortedLeft.zip(sortedRight) { left, right ->
+        abs(left - right)
+    }.sum()
+}
+
+fun two(intLeftSide: List<Int>, intRightSide: List<Int>): Int {
+    val countValuesOfRight = intRightSide.groupingBy { it }.eachCount()
+
+    return intLeftSide.sumOf { countValuesOfRight[it]?.times(it) ?: 0 }
+}
+
+fun main() {
+    val (intLeftSide, intRightSide) = File("inputs/2024-01.txt").readLines().map { line ->
+        val (left, right) = line.split("   ").map { it.toInt() }
+        left to right
+    }.unzip()
+    println(one(intLeftSide, intRightSide))
+    println(two(intLeftSide, intRightSide))
+}
